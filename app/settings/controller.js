1cb8d2d2fadf4f0ae8964470edf598f1034fa332
Lazarev Konstantin <Lazarev-konstant@mail.ru>
2022-03-24 10:27:49 +0200

    add settings controller

--- app/settings/controller.js
+++ app/settings/controller.js
@@ -1,33 +1,39 @@
 const {Router} = require('express')
-const auth = require('./../middleware/auth') 
-const Service = require('../services/SettingsService.js')
+//const auth = require('./../middleware/auth') 
+//const Service = require('../services/SettingsService.js')
 const router = Router()
 
 router.get('/settings', async (req, res) => {
-    let lang = 1
+   /* let lang = 1
     if(req.query.lang) {
         const param = Number(req.query.lang)
         if(Number.isInteger(param)) lang = param
     } 
     const response = await Service.index(lang)
+    */
+    const response = {confirm: 'ok', body: []}
     res.status(200).json(response)
 })
 router.get('/settings/seeds', async (req, res) => {
-    const response = await Service.seeds()
+    //const response = await Service.seeds()
+    const response = {confirm: 'ok', body: []}
     res.status(200).json(response)
 })
-router.post('/admin/settings', auth, async (req, res) => {
-    const {lang} = req.body
-    const response = await Service.indexAdmin(lang)
+router.post('/admin/settings', async (req, res) => {
+    //const {lang} = req.body
+    //const response = await Service.indexAdmin(lang)
+    const response = {confirm: 'ok', body: []}
     res.status(200).json(response)
 })
-router.post('/admin/settings/update', auth, async (req, res) => {
-    const {data} = req.body
-    const response = await Service.update(data)
+router.post('/admin/settings/update', async (req, res) => {
+    //const {data} = req.body
+    //const response = await Service.update(data)
+    const response = {confirm: 'ok', body: []}
     res.status(200).json(response)
 })
-router.post('/admin/settings/:id', auth, async (req, res) => {
-    const response = await Service.show(req.params.id)
+router.post('/admin/settings/:id', async (req, res) => {
+    //const response = await Service.show(req.params.id)
+    const response = {confirm: 'ok', body: []}
     res.status(200).json(response)
 })
 
